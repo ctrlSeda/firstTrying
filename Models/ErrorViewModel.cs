e346f2cce20ba365b85667375d6fdeb126398a27
ctrlSeda <125807609+muhammedgeris12@users.noreply.github.com>
2024-10-15 14:59:45 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace firstTrying.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
